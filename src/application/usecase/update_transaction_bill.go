3bafc7398e810534b8c1a0f6f23c476a29247fae
Jailton Junior <jailton.junior94@outlook.com>
2023-03-20 08:30:13 -0300

    Ajustes na soma das contas de casa

--- src/application/usecase/update_transaction_bill.go
+++ src/application/usecase/update_transaction_bill.go
@@ -26,7 +26,40 @@ func NewUpdateTransactionBill(
 	}
 }
 
+type Transaction struct {
+	Year    int
+	Total   float64
+	Income  float64
+	Outcome float64
+}
+
+func NewTransaction(year int, total, income, outcome float64) *Transaction {
+	return &Transaction{
+		Year:    year,
+		Total:   total,
+		Income:  income,
+		Outcome: outcome,
+	}
+}
+
 func (u *UpdateTransactionBill) Execute() error {
+	// transactions, _ := u.TransactionRepository.GetTransactions("F978F969-3EB6-4D0E-8E4E-3270A20F3513")
+	// transactionsGroup := make(map[int][]*Transaction)
+
+	// for _, t := range transactions {
+	// 	if v, exists := transactionsGroup[t.Date.Year()]; exists {
+	// 		transactionsGroup[t.Date.Year()] = append(v, NewTransaction(t.Date.Year(), t.Total, t.Income, t.Outcome))
+	// 		continue
+	// 	}
+	// 	transactionsGroup[t.Date.Year()] = append(transactionsGroup[t.Date.Year()], NewTransaction(t.Date.Year(), t.Total, t.Income, t.Outcome))
+	// }
+
+	// for _, t := range transactionsGroup {
+	// 	for _, tt := range t {
+	// 		fmt.Printf("ANO: %d | Porcentagem de despesa %v%%\n", tt.Year, math.Round((tt.Outcome/tt.Income)*100))
+	// 	}
+	// }
+
 	bills, err := u.BillRepository.GetBills()
 	if err != nil {
 		return err
@@ -39,7 +72,16 @@ func (u *UpdateTransactionBill) Execute() error {
 			continue
 		}
 
-		r := requests.NewTransactionItemRequest("Casa (Despesas)", "OUTCOME", bill.SixtyPercent)
+		value := 0.0
+		if bill.Date.Year() == 2023 {
+			value = bill.Total / 2
+		}
+
+		if bill.Date.Year() != 2023 {
+			value = bill.Total
+		}
+
+		r := requests.NewTransactionItemRequest("Casa (Despesas)", "OUTCOME", value)
 		if transaction == nil {
 			// t, _ := u.TransactionRepository.GetTransactionByDate(invoice.Date, invoice.Date, "F978F969-3EB6-4D0E-8E4E-3270A20F3513")
 
